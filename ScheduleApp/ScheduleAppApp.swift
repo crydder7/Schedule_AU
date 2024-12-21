7dfaff509c7335f0107c4527fe042e18aa819a96
crydder7 <kaidder7@gmail.com>
2024-12-21 03:13:17 +0300

    Initial Commit

---
+++ ScheduleApp/ScheduleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleAppApp.swift
+//  ScheduleApp
+//
+//  Created by lonely. on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
